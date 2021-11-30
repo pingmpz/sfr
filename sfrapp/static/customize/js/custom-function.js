ed5e5257b71d47ed12478d40fd5b7a017d02034a
pingmpz <pingmpz@hotmail.com>
2021-11-30 07:58:50 +0700

    save

--- sfrapp/static/customize/js/custom-function.js
+++ sfrapp/static/customize/js/custom-function.js
@@ -26,20 +26,6 @@ emptyInputs = (input_ids) => { for(var i = 0;i < input_ids.length;i++) $("#"+inp
 //-- USEFUL FUNCTION
 capitalizeFirstLetter = (str) => str.charAt(0).toUpperCase() + str.slice(1);
 
-function getFullDateTime(datetime) {
-   var result = "";
-   var year = datetime.getFullYear();
-   var month = frontZero((datetime.getMonth() + 1).toString(), 2);
-   var date = frontZero(datetime.getDate().toString(), 2);
-   var hour = frontZero(datetime.getHours().toString(), 2);
-   var minute = frontZero(datetime.getMinutes().toString(), 2);
-   var second = frontZero(datetime.getSeconds().toString(), 2);
-   result = date+"-"+month+"-"+year+", "+hour+":"+minute+":"+second;
-   return result;
-}
-
-getFullDateTimeFromString = (str) => getFullDateTime(new Date(str));
-
 function frontZero(str, length){
   var result = str;
   for(var i = str.length;i < length;i++) result = "0" + result;
@@ -56,4 +42,25 @@ function generateRandomCode(length) {
 
 function roundToTwo(num) {
   return +(Math.round(num + "e+2")  + "e-2");
+}
+
+//-- DATE & TIME
+function getFullDateTime(datetime) {
+   var result = "";
+   var year = datetime.getFullYear();
+   var month = frontZero((datetime.getMonth() + 1).toString(), 2);
+   var date = frontZero(datetime.getDate().toString(), 2);
+   var hour = frontZero(datetime.getHours().toString(), 2);
+   var minute = frontZero(datetime.getMinutes().toString(), 2);
+   var second = frontZero(datetime.getSeconds().toString(), 2);
+   result = date+"-"+month+"-"+year+", "+hour+":"+minute+":"+second;
+   return result;
+}
+
+getFullDateTimeFromString = (str) => getFullDateTime(new Date(str));
+
+function getDiffMin(start_date_time, stop_date_time){
+  var diff = Math.abs(stop_date_time - start_date_time);
+  var minutes = Math.floor((diff/1000)/60);
+  return minutes;
 }
